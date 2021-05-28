aa54f1790d8d2058c3343242bf5e1faf08ab9ce8
katredford <katharine.redford@gmail.com>
2021-05-28 09:35:14 -0500

    first commit

---
+++ Develop/routes.js
@@ -0,0 +1,11 @@
+module.exports = function(app) {
+
+    // app.get('/home', (req, res) => {
+    //     res.sendFile('/Users/katier./Desktop/Bootcamp/noteTaker.redford/miniature-eureka/Develop/public/index.html')
+    // })
+
+    // app.get('/notes', (req, res) => {
+    //     res.sendFile('/Users/katier./Desktop/Bootcamp/noteTaker.redford/miniature-eureka/Develop/public/notes.html')
+    // })
+
+}
---
+++ Develop/server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3001;
+const fs = require('fs')
+app.use(express.static('public'));
+
+// require('./routes')(app)
+
+app.get('/home', (req, res) => {
+    res.sendFile('/Users/katier./Desktop/Bootcamp/noteTaker.redford/miniature-eureka/Develop/public/index.html')
+})
+
+app.get('/notes', (req, res) => {
+    res.sendFile('/Users/katier./Desktop/Bootcamp/noteTaker.redford/miniature-eureka/Develop/public/notes.html')
+})
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', function(err, result) {
+        console.log('resultttt', result)
+        res.json(JSON.parse(result))
+    })
+})
+
+// adding a note when we submit the form!!
+// req.body will the the stuff form the form!!!!
+// fs.read in json file
+// add id to req.body
+// (id = results.lenght + 1)
+// push req.body into array we got from reading in json file
+// fs.writeFile the new array to the json file
+
+// deleteing a note when we hit hte trash can
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
+})
